d233a8f8910468743a1d582e7bbb41f4c8c68214
douglasrfferreira <douglas_ferreira@edu.unifil.br>
2025-12-02 20:29:50 -0300

    Implementa classe dividir

--- untitled/src/Dividir.java
+++ untitled/src/Dividir.java
@@ -1,2 +1,8 @@
-public class Dividir {
+class Dividir implements Operacao {
+    public double executar(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Não é possível dividir por zero!");
+        }
+        return a/b;
+    }
 }
